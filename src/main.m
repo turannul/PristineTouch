35c926a1a55b1081cb3e18a3ba43c7c093c38fb9
Turann_ <84424530+turannul@users.noreply.github.com>
2023-06-30 23:35:38 +0300

    Update main.m

--- src/main.m
+++ src/main.m
@@ -1,11 +1,28 @@
+//
+//  main.m
+//  PristineTouch
+//  Original author: github.com/gltchitm
+//  Recreated by Turann_ on 30.06.2023.
+//
+
 #import <Cocoa/Cocoa.h>
-#import "AppDelegate.m"
+#import "AppDelegate.h"
 
-int main(void) {
-    NSApplication* application = [NSApplication sharedApplication];
-
-    AppDelegate* applicationDelegate = [[AppDelegate alloc] init];
-    [application setDelegate:applicationDelegate];
-
-    [application run];
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSApplication *application = [NSApplication sharedApplication];
+        AppDelegate *appDelegate = [[AppDelegate alloc] init];
+        [application setDelegate:appDelegate];
+        
+        NSWindow *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 200, 200)
+                                                       styleMask:NSWindowStyleMaskTitled
+                                                         backing:NSBackingStoreBuffered
+                                                           defer:NO];
+        [window setTitle:@"Keyboard Cleaner"];
+        [window makeKeyAndOrderFront:nil];
+        
+        [application run];
+    }
+    
+    return NSApplicationMain(argc, argv);
 }
