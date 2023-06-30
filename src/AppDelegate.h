7712c7d10404fd64aba6ae7e99e522ccf60c3257
Turann_ <84424530+turannul@users.noreply.github.com>
2023-06-30 23:35:28 +0300

    Update AppDelegate.h

--- src/AppDelegate.h
+++ src/AppDelegate.h
@@ -1,5 +1,12 @@
+//
+//  AppDelegate.h
+//  PristineTouch
+//  Original author: github.com/gltchitm
+//  Recreated by Turann_ on 30.06.2023.
+//
+
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject<NSApplicationDelegate>
-@property (retain) NSWindow* window;
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+@property (strong, nonatomic) NSWindow *window;
 @end
